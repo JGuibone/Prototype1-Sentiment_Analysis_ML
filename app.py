d315abbb71bf7d3c1409a511fcd6b33dff161d4b
JGuibone <114382307+JGuibone@users.noreply.github.com>
2023-05-17 15:49:42 +0800

    update

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-from flask import request
-from flask import Flask, render_template
-from pyScripts.SentimentFunctions import stringSentement
-
-app = Flask(__name__)
+from website import create_app
 
 
-@app.route('/', methods=['POST','GET'])
-def my_form_post():
-
-    if request.method == 'POST':
-        text = request.form['text']
-        results = stringSentement(text)
-        return render_template('index.html', variable=results)
-    else:
-        return render_template('index.html')
+app = create_app()
 
 
-if __name__ == "__main__":
-    app.run(port='8088', threaded=False, debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ website/__init__.py
@@ -0,0 +1,35 @@
+from flask import request
+from flask import Flask, render_template
+from website.pyScripts.SentimentFunctions import stringSentement
+
+
+def create_app():
+
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'EO0UzmL6NVcL!t2&HlkW*@$h7dHfbjCIl*K*jSjZP@RX*UTM2$vGBV^hBQ*^I5FD'
+
+    # @app.route('/', methods=['POST','GET'])
+    # def my_form_post():
+
+    #     if request.method == 'POST':
+    #         text = request.form['text']
+    #         results = stringSentement(text)
+    #         return render_template('index.html', variable=results)
+    #     else:
+    #         return render_template('index.html')
+    
+    @app.route("/", methods=['POST','GET'])
+    def index():
+        # return 'testing route'
+        return render_template('index.html')
+    
+    @app.route("/results")
+    def results():
+        # return 'testing route'
+        return render_template('results.html')
+
+    @app.route("/test")
+    def testroute():
+        return 'testroute'
+
+    return app
